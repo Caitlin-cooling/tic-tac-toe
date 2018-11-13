b187af97e3490f209655c933e26f69a181d81fa5
Caitlin Cooling <ccooling97@gmail.com>
2018-11-13 15:41:38 +0000

    Cordinates of the square that the user clicked are now saved They are then used to set the move

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
   var game = new Game;
+  var cordinates= "";
 
   checkPlayer();
 
-  $('button').click(function(){
+  $('button').click(function(e){
+    cordinates = e.target.id;
     game.switchPlayer();
     checkPlayer();
+    setPlayerMove();
   });
 
   function checkPlayer() {
     $('#player').text(game.playerTurn())
+  }
+
+  function setPlayerMove(){
+    game.setMove(cordinates)
   }
 });
