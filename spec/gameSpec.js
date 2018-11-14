a8f427cc29ca94cd8bd9e230679649b1f3b69084
Caitlin Cooling <ccooling97@gmail.com>
2018-11-14 08:43:17 +0000

    Game can now determine a winner

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -17,10 +17,49 @@ describe("game", function(){
   });
 
   describe("getMoves", function(){
-    it("gets the moves of a player", function(){
-      game.setMove("3,3")
-      game.setMove("1,3")
-      expect(game.getMoves()).toEqual(["3,3", "1,3"])
+    it("gets the moves of the player X", function(){
+      game.setMove("a3")
+      expect(game.getMovesX()).toEqual(["a3"])
+    });
+
+    it("gets the moves of the player O", function(){
+      game.setMove("a3")
+      game.setMove("c3")
+      expect(game.getMovesO()).toEqual(["c3"])
+    });
+  });
+
+  describe("checkWinner", function(){
+    it("determines that the winner is X horizontally", function(){
+      game.setMove("a3")
+      game.setMove("c3")
+      game.setMove("a2")
+      game.setMove("b2")
+      game.setMove("a1")
+      expect(game.checkWinner()).toBe("X")
+    });
+
+    it("determines that the winner is O vertically", function(){
+      game.setMove("a1")
+      game.setMove("a3")
+      game.setMove("b2")
+      game.setMove("b3")
+      game.setMove("b1")
+      game.setMove("c3")
+      expect(game.checkWinner()).toBe("O")
+    });
+
+    it("determines a draw when the board is full", function(){
+      game.setMove("a3")
+      game.setMove("a2")
+      game.setMove("a1")
+      game.setMove("b3")
+      game.setMove("b2")
+      game.setMove("b1")
+      game.setMove("c3")
+      game.setMove("a2")
+      game.setMove("a1")
+      expect(game.checkWinner()).toBe("draw")
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 Game = function() {
   this.player = "X";
-  this.moves = [];
+  this.xMoves = [];
+  this.oMoves = [];
+  this.moveCount = 0;
+  this.winningCombos = ("a1" && "a2" && "a3") || ("b1" && "b2" && "b3") || ("c1" && "c2" && "c3") || ("a3" && "b3" && "c3") || ("a2" && "b2" && "c2") || ("a1" && "b1" && "c1") || ("a1" && "b2" && "c3") || ("a3" && "b2" && "c1")
 };
 
 Game.prototype.playerTurn = function () {
@@ -15,10 +18,29 @@ Game.prototype.switchPlayer = function () {
   }
 };
 
-Game.prototype.setMove = function(cordinates) {
-  this.moves.push(cordinates);
+Game.prototype.setMove = function(move) {
+  if(this.player === "X") {
+    this.xMoves.push(move);
+  } else {
+    this.oMoves.push(move);
+  }
+  ++ this.moveCount;
+  this.switchPlayer();
 }
 
-Game.prototype.getMoves = function(cordinates) {
-  return this.moves;
+Game.prototype.getMovesX = function(move) {
+  return this.xMoves;
 }
+
+Game.prototype.getMovesO = function(move) {
+  return this.oMoves;
+}
+
+Game.prototype.checkWinner = function () {
+  if(this.moveCount === 9) {return "draw"}
+  if(this.xMoves.includes(this.winningCombos)){
+    return "X"
+  } else if(this.oMoves.includes(this.winningCombos)){
+    return "O"
+  }
+};
